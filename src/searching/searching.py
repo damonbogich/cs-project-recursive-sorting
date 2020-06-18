50c98a1bfa0399c02dfdbaf24c763ed9afdd23fa
damonbogich <damonbogich@gmail.com>
2020-06-18 16:27:21 -0400

    binary search almost working

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,47 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    start = 0 
+    #if test doesn't pass, it's possible you don't need to set
+    #start and end variables
+    start = 0
     end = len(arr) - 1
-    
+
     middle = (start + end) // 2
+    # 3 rules:
+        #1. must have base case:
+            # array is narrowed down to one item
+            # indexes value either = target or not
+    if len(arr) <= 1 and arr[middle] == target:
+        return middle
+    if len(arr) <= 1 and arr[middle] != target:
+        return -1
+    #set up for list that still needs to split
+    #if guess to the middle of list is correct return middle
+    if len(arr) > 1 and arr[middle] == target:
+        return middle
+    #if length of array is greater than one and middle > target
+    if len(arr) > 1 and arr[middle] > target:
+        #now we need to split the array so it only contains the indices
+        #middle + 1 through end
+        arr = arr[start:middle]
+        return binary_search(arr, target, start, end)
+    if len(arr) > 1 and arr[middle] < target:
+        #get rid of middle and everything below it
+        arr = arr[middle+1:end+1]
+        return binary_search(arr, target, start, end)
+
+arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+binary_search(arr1, -8, 0, len(arr1)-1 )
+
+
+    #if length of array is greater than one and middle < target
+
+
+    
+        
+
+        #2. must move twoards base case
+        #3. must call itself.
+    
 
 
 
